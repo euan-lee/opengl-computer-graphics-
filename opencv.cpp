743c8b44ad289f36afdebeab89c80c2e0109b596
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-11-15 21:49:03 +0900

    Update opencv.cpp

--- opencv.cpp
+++ opencv.cpp
@@ -23,13 +23,20 @@ void Affine() {
 	srcTri[0] = Point2f(0, 0);
 	srcTri[1] = Point2f(src.cols - 1.0f, 0);
 	srcTri[2] = Point2f(0, src.rows - 1.0f);
-	dstTri[0] = Point2f(src.cols*0.0f, src.rows*0.33f);
-	dstTri[1] = Point2f(src.cols*0.85f, src.rows*0.25f);
-	dstTri[2] = Point2f(src.cols*0.15f, src.rows*0.7f);
+	dstTri[0] = Point2f(x_Arr[0], y_Arr[0]);
+	dstTri[1] = Point2f(x_Arr[1], y_Arr[1]);
+	dstTri[2] = Point2f(x_Arr[2], y_Arr[2]);
 	warp_mat = getAffineTransform(srcTri, dstTri);
 	warpAffine(src, dst, warp_mat, src.size());
-	imshow("src", src);   imshow("dst", dst);
-	waitKey(0);
+	resize(Frame_image, Frame_image, Size(lenna_image.cols, lenna_image.rows));
+	imshow("dst", dst);
+	imshow("frame", Frame_image);
+	double alpha, beta;
+	Mat Fusion;
+	alpha=0.4;
+	beta = (1.0 - alpha);
+	addWeighted(Frame_image, alpha, dst, beta, 0.0, Fusion);
+	imshow("선형 합성", Fusion);
 	printf("affine");
 }
 
@@ -45,6 +52,7 @@ void onMouse(int event, int x, int y, int flags, void* param) // 마우스 콜�
 		}
 		if (click_cnt == 3) {
 			Affine();
+			click_cnt = -1;
 		}
 		click_cnt++;
 	}
@@ -54,14 +62,21 @@ int Opencv_project1() {
 	lenna_image = imread("lenna.jpg");
 	Frame_image = imread("Frame1.jpg");
 	imshow("image", lenna_image);
-	imshow("image_frame", Frame_image);
 	setMouseCallback("image", onMouse);
 	return 0;
 }
-
 void Opencv_project2() {
+	printf("project2");
+}
+void OpencvSave() {
+	printf("save");
 }
 
+
+void Exit() {
+	exit(0);
+	printf("exit");
+}
 void control_cb(int control)
 {
 	if (control == 0) {
@@ -69,6 +84,12 @@ void control_cb(int control)
 	}
 	if (control == 1) {
 		Opencv_project2();
+	}
+	if (control == 2) {
+		OpencvSave();
+	}
+	if (control == 3) {
+		Exit();
 	}
 }
 
@@ -81,8 +102,10 @@ int main()
 	int main_window = glui->get_glut_window_id();
 	new GLUI_StaticText(glui, "컴비전 과제");
 	new GLUI_Separator(glui);
-	checkbox = new GLUI_Checkbox(glui, "case1", &wireframe, 0, control_cb);
-	checkbox = new GLUI_Checkbox(glui, "case2", &wireframe, 1, control_cb);
+	checkbox = new GLUI_Checkbox(glui, "assignment1", &wireframe, 0, control_cb);
+	checkbox = new GLUI_Checkbox(glui, "assignment2", &wireframe, 1, control_cb);
+	checkbox = new GLUI_Checkbox(glui, "save", &wireframe, 2, control_cb);
+	checkbox = new GLUI_Checkbox(glui, "exit", &wireframe, 3, control_cb);
 	glutMainLoop();
 	return 0;
 }
